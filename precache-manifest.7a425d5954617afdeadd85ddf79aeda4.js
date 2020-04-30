dff29e03bece20711bc3109f4ac82ea1d5b0d3b9
Ronnasayd Machado <ronnasayd@hotmail.com>
2020-04-30 18:43:10 -0300

    Updates

---
+++ precache-manifest.7a425d5954617afdeadd85ddf79aeda4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eed36801ebb4fa3175483fb16810f9aa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e83973211458edb35dc6",
+    "url": "/static/js/2.60ced003.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.60ced003.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c8cb891e61235d14e11e",
+    "url": "/static/js/main.587a045a.chunk.js"
+  },
+  {
+    "revision": "9044a345aeebfb59e96c",
+    "url": "/static/js/runtime-main.019f1e69.js"
+  }
+]);
