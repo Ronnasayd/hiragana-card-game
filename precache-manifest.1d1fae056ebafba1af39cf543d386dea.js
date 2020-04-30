8c4d3c01eb740f8c016b80f7e1718879c2be5bb5
Ronnasayd Machado <ronnasayd@hotmail.com>
2020-04-30 18:46:17 -0300

    Updates

---
+++ precache-manifest.1d1fae056ebafba1af39cf543d386dea.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e421382ce8742a023566962fc886629",
+    "url": "/hiragana-card-game/index.html"
+  },
+  {
+    "revision": "0f51106d9786a254f65b",
+    "url": "/hiragana-card-game/static/js/2.7d8692ad.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/hiragana-card-game/static/js/2.7d8692ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c8cb891e61235d14e11e",
+    "url": "/hiragana-card-game/static/js/main.587a045a.chunk.js"
+  },
+  {
+    "revision": "a948c7550cf7e03ffa5d",
+    "url": "/hiragana-card-game/static/js/runtime-main.d201db79.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7a425d5954617afdeadd85ddf79aeda4.js"
+  "/hiragana-card-game/precache-manifest.1d1fae056ebafba1af39cf543d386dea.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/hiragana-card-game/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
